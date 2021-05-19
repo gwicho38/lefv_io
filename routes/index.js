49d6226232a19fdd4ac95de4a4328bdf4da73c9f
gwicho38 <lef16b@my.fsu.edu>
2021-05-18 20:18:10 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'lefv | io' });
+  res.render('index', { title: 'lefv' });
 });
 
 module.exports = router;
