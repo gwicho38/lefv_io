2481ab13484eaa7ecf68df784cfd426ed104dff3
Luis E. Fernandez de la Vara <lef16b@my.fsu.edu>
2022-03-25 22:06:33 -0400

    To polish index page.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
 import Layout from '../../components/layout'
+import Head from 'next/head'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticProps({ params }) {
     const postData = await getPostData(params.id); 
@@ -21,13 +24,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
