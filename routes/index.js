8c2c4d0ff63b66a4e90844deb43770355aee2d93
Luis E. Fernandez de la Vara <lefv@localhost.localdomain>
2024-02-10 12:56:53 -0500

    Initial commit with pohtml

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
+var logger = require('morgan');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/test', function (req, res, next) {
+  logger.log("test");
   res.render('index', { title: 'lefv' });
+
 });
 
 module.exports = router;
